879b54416b6f20247c2c75d669786b34822ecd5a
Johnson-8 <47229625+Johnson-8@users.noreply.github.com>
2022-09-14 22:17:12 -0600

    first commit

---
+++ get.py
@@ -0,0 +1,6 @@
+import requests
+
+# for q2
+print("Requests version: " + requests.__version__ + "\n")
+
+print(requests.get("http//google.com"))
---
+++ getAnswer.py
@@ -0,0 +1,8 @@
+import requests
+
+# virtualevn.4
+print("Requests version: " + requests.__version__ + "\n")
+
+# for curl.5
+r = requests.get("http://www.google.com")
+print("response: " + str(r) + "\n" + "text: " + str(r.text))
