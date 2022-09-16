8fafbb06b8846ccae07d5922cdffe58953804612
Johnson-8 <47229625+Johnson-8@users.noreply.github.com>
2022-09-15 21:35:10 -0600

    add selfdownloading script

--- getAnswer.py
+++ getAnswer.py
@@ -5,4 +5,18 @@ print("Requests version: " + requests.__version__ + "\n")
 
 # for curl.5
 r = requests.get("http://www.google.com")
-print("response: " + str(r) + "\n" + "text: " + str(r.text))
+print("cURL response: " + str(r))
+#print("response: " + str(r) + "\n" + "text: " + str(r.text))
+
+
+# download itself
+fileURL = "https://raw.githubusercontent.com/Johnson-8/CMPUT_404/master/getAnswer.py"
+r = requests.get(fileURL)
+file = open("downloadMyself.py", "w")
+file.write("\nMy source code: \n" + str(r.text))
+file.close()
+
+# print out source code
+file = open("downloadMyself.py", "r")
+print(file.read())
+file.close()
