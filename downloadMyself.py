7a18837e3f2458e91baebd2fb612bf7a66083975
Johnson-8 <47229625+Johnson-8@users.noreply.github.com>
2022-09-15 21:44:49 -0600

    fixed script

--- downloadMyself.py
+++ downloadMyself.py
@@ -1,5 +1,5 @@
 
- My source code: 
+My source code: 
 import requests
 
 # virtualevn.4
@@ -7,4 +7,18 @@ print("Requests version: " + requests.__version__ + "\n")
 
 # for curl.5
 r = requests.get("http://www.google.com")
-print("response: " + str(r) + "\n" + "text: " + str(r.text))
+print("cURL response: " + str(r))
+#print("response: " + str(r) + "\n" + "text: " + str(r.text))
+
+
+# download itself
+fileURL = "https://raw.githubusercontent.com/Johnson-8/CMPUT_404/master/getAnswer.py"
+r = requests.get(fileURL)
+file = open("downloadMyself.py", "w")
+file.write("\nMy source code: \n" + str(r.text))
+file.close()
+
+# print out source code
+file = open("downloadMyself.py", "r")
+print(file.read())
+file.close()
--- getAnswer.py
+++ getAnswer.py
@@ -1,22 +1,21 @@
 import requests
 
 # virtualevn.4
-print("Requests version: " + requests.__version__ + "\n")
+print("Requests version: " + requests.__version__)
 
 # for curl.5
 r = requests.get("http://www.google.com")
 print("cURL response: " + str(r))
 #print("response: " + str(r) + "\n" + "text: " + str(r.text))
 
-
 # download itself
 fileURL = "https://raw.githubusercontent.com/Johnson-8/CMPUT_404/master/getAnswer.py"
 r = requests.get(fileURL)
 file = open("downloadMyself.py", "w")
-file.write("\nMy source code: \n" + str(r.text))
+file.write(r.text)
 file.close()
 
 # print out source code
 file = open("downloadMyself.py", "r")
-print(file.read())
+print("My source code:\n" + str(file.read()))
 file.close()
